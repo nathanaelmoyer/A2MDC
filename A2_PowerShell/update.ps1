d848c65a07e9350c29817b76a51b8937379dba0b
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2023-05-12 17:04:14 -0400

    Create update.ps1
    simple script to update existing ps1

---
+++ A2_PowerShell/update.ps1
@@ -0,0 +1,3 @@
+#https://github.com/nathanaelmoyer/A2MDC/releases/latest/download/1.A2_MDC_Automation.ps1
+
+Invoke-WebRequest https://github.com/nathanaelmoyer/A2MDC/releases/latest/download/1.A2_MDC_Automation.ps1 -OutFile "1. A2_MDC_Automation.ps1"
